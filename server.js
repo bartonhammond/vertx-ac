fa075b51160aeb0cb9843a8a4a400ec04632717b
Barton Hammond <bartonhammond@gmail.com>
2012-09-11 18:05:12 -0500

    Serve index

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ load('vertx.js');
 var server = vertx.createHttpServer();
 
 server.requestHandler(function(req) {
+  if (req.uri == "/") {
+      req.response.sendFile("index.html");
+  }
   if (req.uri == "/port") {
       req.response.end("Port: " + vertx.env['PORT']);
   }
