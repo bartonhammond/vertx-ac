f44180b61d9389d384a8a78ae7b4b42a8fc6d9ea
Barton Hammond <bartonhammond@gmail.com>
2012-09-11 23:33:29 +0100

    Figure port

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ load('vertx.js');
 
 var server = vertx.createHttpServer();
 
+server.requestHandler(function(req) {
+  if (req.uri == "/port") {
+      req.response.end("Port: " + vertx.env['PORT']);
+  }
+
+})
 // Create a SockJS bridge which lets everything through (be careful!)
 vertx.createSockJSServer(server).bridge({prefix: "/eventbus"}, [{}], [{}]);
+
 
 server.listen(vertx.env['PORT'],'0.0.0.0');
