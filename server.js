3cc7c6c5f735d5536e8f16f95122fe12e233ea1e
Barton Hammond <bartonhammond@gmail.com>
2012-09-12 14:14:33 +0100

    Remove index and update server Add README

--- server.js
+++ server.js
@@ -5,5 +5,9 @@ var server = vertx.createHttpServer();
 // Create a SockJS bridge which lets everything through
 vertx.createSockJSServer(server).bridge({prefix: "/eventbus"}, [{}], [{}]);
 
+//Start the server, list to the port defined in the environment and 
+//accept requests from any host.
+//When running locally, set the PORT to say 8080
+//It's purpose has more to with running on Heroku
 server.listen(vertx.env['PORT'],'0.0.0.0');
 
